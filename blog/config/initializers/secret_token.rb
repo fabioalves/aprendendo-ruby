ee3608bc346f41053f0db0b31ee5f2425b625feb
Fabio Alves <fabioalves101@gmail.com>
2012-06-01 00:24:35 -0400

    Criacao da aplicacao blog. Teste de scaffold

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '3bdf44eadd8a1b88df73e4b0dbeda77af05988c36896c9f487d5f493927dd73b9a0ea230bfc3ae64567527b2fe0f7030fc36846a13d699eb39d13fc22840cf90'
